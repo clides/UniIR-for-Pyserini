32dae4c23c09043d6a3cb28658859f53d4d068e5
Jimmy Lin <jimmylin@uwaterloo.ca>
2026-05-05 19:30:02 -0400

    [codex] Add Transformers 5 compatibility (#17)

--- src/models/uniir_blip/backbone/med.py
+++ src/models/uniir_blip/backbone/med.py
@@ -36,18 +36,20 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
+from transformers.pytorch_utils import apply_chunking_to_forward, prune_linear_layer
+
 try:
-    from transformers import apply_chunking_to_forward
-    from transformers.pytorch_utils import (
-        find_pruneable_heads_and_indices,
-        prune_linear_layer,
-    )
-except:
-    from transformers.modeling_utils import (
-        apply_chunking_to_forward,
-        find_pruneable_heads_and_indices,
-        prune_linear_layer,
-    )
+    from transformers.pytorch_utils import find_pruneable_heads_and_indices
+except ImportError:
+    def find_pruneable_heads_and_indices(heads, n_heads, head_size, already_pruned_heads):
+        heads = set(heads) - already_pruned_heads
+        mask = torch.ones(n_heads, head_size)
+        for head in heads:
+            head = head - sum(1 if h < head else 0 for h in already_pruned_heads)
+            mask[head] = 0
+        mask = mask.view(-1).contiguous().eq(1)
+        index = torch.arange(len(mask))[mask].long()
+        return heads, index
 from transformers.modeling_utils import (
     PreTrainedModel,
 )
